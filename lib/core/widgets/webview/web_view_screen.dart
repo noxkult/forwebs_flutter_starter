7853a6f7dede364eb5c345e48f957d3e8009ced0
jungho <space@forwebs.co.kr>
2026-08-29 20:19:24 +0900

    Fix PlatformException import

--- lib/core/widgets/webview/web_view_screen.dart
+++ lib/core/widgets/webview/web_view_screen.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 /// Displays [url] in a full-screen web view.
